ef9efaa993d6d72dbfe056e1558ccb5044337eb6
hudsongoldal <hudson.goldal@ndsu.edu>
2026-04-02 11:06:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,6 @@ csv = df.to_csv().encode("utf-8")
 st.download_button(
     label="Download Data as CSV",
     data = csv,
-    file_name=f"{ticker}_stock_data.csv"
+    file_name=f"{ticker}_stock_data.csv",
     mime="text/csv"
 )
