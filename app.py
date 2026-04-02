6bc6a74d6911e1db6c6f35a237fa8cdac7f24d68
hudsongoldal <hudson.goldal@ndsu.edu>
2026-04-02 11:01:00 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title = "Stock Data Extraction App",layout = "wide")
+
+st.title("Stock Data Extraction App")
+
+st.write(Extract stock market prices from Yahoo Finance using ticker symbol)
+
+st.sidebar.header("User input")
+
+ticker = st.sidebar.text_input("Enter Ticker","AAPL") 
+
+start_date = st.sidebar.date_input("Start Date",pd.to_datetime("2023"))
+
+end_date = st.sidebar.date_input("End Date",pd.to_datetime("today"))
+
+if st.sidebar.button("Get Data"):
+  stock = yf.Ticker(ticker)
+  df = stock.history(start=start_date, end = end_date)
+  
+  if df.empty:
+    st.error("No data found. Please check the ticker symbol or date range.")
+   
+    else:
+      st.success(f"Data successfully extracted for {ticker}")
+
+st.subheader("Company Information")
+info=stock.info
+
+company_name = info.get("longName","N/A")
+sector = info.get("sector","N/A")
+industry = info.get("industry","N/A")
+Market_Cap = ("marketcap","N/A")
+website=info.get("website","N/A")
+
+st.write(f"**Company Name:** {company_name}")
+st.write(f"**Sector:** {sector}")
+st.write(f"**Industry:** {industry}")
+st.write(f"**Market Cap:** {market_cap}")
+st.write(f"**Website:** {website}")
+
+st.subheader("Closing Price Chart")
+st.dataframe(df)
+
+st.subheader("Closing Price Chart")
+fig,ax = plt.subplots()
+ax.plot(df.index,df["Close"])
+ax.set_xlabel("Date")
+ax.set_ylabel("Closing Price")
+ax.set_title(f"{ticker} Closing Price")
+st.pyplot(fig)
+
+csv = df.to_csv().encode("utf-8")
+
+st.download_button(
+    label="Download Data as CSV",
+    data=csv
+    file_name=f"{ticker}_stock_data.csv"
+    mime="text/csv"
+)
